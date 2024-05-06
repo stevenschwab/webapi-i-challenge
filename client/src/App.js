dacc981c685c7216d41d3ce6aeaf8439dc6731d1
Steve Schwab <schwabs@umich.edu>
2024-05-06 13:29:45 -0700

    delete unnecessary html element from app component and add h1 header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -28,21 +26,8 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users List</h1>
     </div>
   );
 }
